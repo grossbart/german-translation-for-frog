67f6862630aff93978a08b52985883434375ce02
Peter Gassner <peter@naehrstoff.ch>
2009-09-21 15:25:12 +0200

    German language file for comment plugin by Tobias Rieder.

---
+++ frog/plugins/comment/i18n/de-message.php
@@ -0,0 +1,60 @@
+<?php
+
+    /**
+     * German language file for plugin comment
+     *
+     * @package frog
+     * @subpackage plugin.comment.translations
+     *
+     * @author Tobias Rieder <tobi@antary.de>
+     * @version Frog 0.9.5
+     */
+
+    return array(
+    'A comment was added.' => 'Ein Kommentar wurde hinzugefügt.',
+	'Approve' => 'Genehmigen',
+	'Are you sure you wish to delete it?' => 'Willst du den Kommentar wirklich löschen?',
+	'Auto approve' => 'Automatische Freischaltung',
+	'Body' => 'Kommentar',
+	'Cancel' => 'Abbrechen',
+	'Choose yes if you want to use a captcha to protect yourself against spammers.' => 'Ja auswählen, um ein Captcha zum Schutz gegen Spammer zu verwenden.',
+	'Choose yes if you want your comments to be auto approved. Otherwise, they will be placed in the moderation queue.' => 'Ja auswählen, um den Kommentar automatisch freizuschalten. Ansonsten wird der Kommentar in die Moderation gelegt.',
+	'Closed' => 'Geschlossen',
+	'Comment has been approved!' => 'Der Kommentar wurde genehmigt!',
+	'Comment has been deleted!' => 'Der Kommentar wurde gelöscht!',
+	'Comment has been saved!' => 'Der Kommentar wurde gespeichert!',
+	'Comment has been unapproved!' => 'Der Kommentar wurde zurückgewiesen!',
+	'Comment has not been deleted!' => 'Der Kommentar wurde nicht gelöscht!',
+	'Comment has not been saved!' => 'Der Kommentar wurde nicht gespeichert!',
+	'Comment not found!' => 'Kommentar nicht gefunden!',
+	'Comments' => 'Kommentare',
+	'Comments Plugin' => 'Kommentarplugin',
+	'Comments per page' => 'Kommentare pro Seite',
+	'Comments settings' => 'Kommentareinstellungen',
+	'Delete' => 'Löschen',
+	'Documentation' => 'Dokumentation',
+	'Edit comment' => 'Kommentar bearbeiten',
+	'Enhance comments tab' => 'Kommentar-Tab ',
+	'Incorrect result value. Please try again:' => 'Falsches Ergebnis. Bitte erneut versuchen:',
+	'Moderation' => 'Moderation',
+	'Modify' => 'Bearbeiten',
+	'No' => 'Nein',
+	'No comments found for moderation.' => 'Keine Kommentare für die Moderation gefunden.',
+	'No comments found.' => 'Keine Kommentare gefunden.',
+	'Open' => 'Öffnen',
+	'Please insert the result of the arithmetical operation from the following image:' => 'Bitte das Ergebnis der folgenden mathematischen Operation eingeben:',
+	'Please insert the result of the arithmetical operation from this image.' => 'Bitte das Ergebnis der mathematischen Operation auf diesem Bild eingeben.',
+	'Reject' => 'Zurückweisen',
+	'Save' => 'Speichern',
+	'Sets the number of comments to be displayed per page in the backend.' => 'Die Anzahl der Kommentare, die im Backend angezeigt werden.',
+	'Settings' => 'Einstellungen',
+	'Thank you for your comment. It has been added.' => 'Vielen Dank für den Kommentar. Er wurde hinzugefügt.',
+	'Thank you for your comment. It is waiting for approval.' => 'Vielen Dank für den Kommentar. Er muss noch überprüft werden.',
+	'The settings have been updated.' => 'Die Einstellungen wurden gespeichert.',
+	'Use captcha' => 'Captcha benutzen',
+	'Yes' => 'Ja',
+	'about' => 'über',
+	'comment not found!' => 'Kommentar nicht gefunden!',
+	'none' => 'kein',
+	'or' => 'oder',
+    );
